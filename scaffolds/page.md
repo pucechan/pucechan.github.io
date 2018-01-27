ddceb83ca045b738067e7bd7d7a504bddc4f1a9a
Simon <pucechan@gmail.com>
2018-01-27 21:40:17 +0000

    Testing

--- scaffolds/page.md
+++
@@ -1,7 +0,0 @@
----
-title: {{ title }}
-date: {{ date }}
-cover_image:
-category: 
-tags: 
----
--- themes/pucechan/source/js/highlight.js
+++
@@ -1,6 +0,0 @@
-  document.addEventListener("DOMContentLoaded", function(event) {
-    var codeBlocks = Array.prototype.slice.call(document.getElementsByTagName('pre'))
-    codeBlocks.forEach(function(block, index) {
-      hljs.highlightBlock(block);
-    });
-  });
